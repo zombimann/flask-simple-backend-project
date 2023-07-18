adf4bd6d2c268e22b30a82e3e924a669e0690a1b
Mugambi Ndwiga <mugambindwiga@gmail.com>
2023-07-18 17:27:54 +0300

    remove sensitive information

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://zombi:C0mpradors*@localhost/mydb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/mydb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
